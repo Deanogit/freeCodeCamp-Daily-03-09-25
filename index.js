e593fb3865a5829abf79b5693ee087d7100401f6
Dean Howe <hideanhowe@gmail.com>
2025-12-18 16:11:47 +0000

    solution

--- index.js
+++ index.js
@@ -12,3 +12,19 @@
 // For example, given "1. My item", return "<li>My item</li>".
 
 // Note: The console may not display HTML tags in strings when logging messages. Check the browser console to see logs with tags included.
+
+function convertListItem(markdown) {
+  console.log(markdown);
+  // zero or more spaces
+  const regex = /\s*\d\.\s+/;
+  if (regex.test(markdown)) {
+    console.log('Hello');
+    console.log(markdown.replace(regex, '<li>'));
+    let test = markdown.replace(regex, '<li>');
+    console.log(test);
+    return test + '</li>';
+  } else {
+    return 'Invalid format';
+  }
+  return markdown;
+}
